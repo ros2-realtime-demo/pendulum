d566bbbf86c9f187acf1f0555619f0125149b688
Carlos San Vicente <carlos.svic@gmail.com>
2019-09-25 19:05:46 +0200

    added rttest real-time settings in test executables

--- pendulum_motor/test/test_pendulum_motor.cpp
+++ pendulum_motor/test/test_pendulum_motor.cpp
@@ -37,6 +37,25 @@ int main(int argc, char * argv[])
             rclcpp::NodeOptions().use_intra_process_comms(true));
 
     exec.add_node(motor_node->get_node_base_interface());
+
+    // Set the priority of this thread to the maximum safe value, and set its scheduling policy to a
+    // deterministic (real-time safe) algorithm, round robin.
+    if (rttest_set_sched_priority(98, SCHED_RR)) {
+      perror("Couldn't set scheduling priority and policy");
+    }
+
+    // Lock the currently cached virtual memory into RAM, as well as any future memory allocations,
+    // and do our best to prefault the locked memory to prevent future pagefaults.
+    // Will return with a non-zero error code if something went wrong (insufficient resources or
+    // permissions).
+    // Always do this as the last step of the initialization phase.
+    // See README.md for instructions on setting permissions.
+    // See rttest/rttest.cpp for more details.
+    if (rttest_lock_and_prefault_dynamic() != 0) {
+      fprintf(stderr, "Couldn't lock all cached virtual memory.\n");
+      fprintf(stderr, "Pagefaults from reading pages not yet mapped into RAM will be recorded.\n");
+    }
+
     exec.spin();
     rclcpp::shutdown();
     return EXIT_SUCCESS;
