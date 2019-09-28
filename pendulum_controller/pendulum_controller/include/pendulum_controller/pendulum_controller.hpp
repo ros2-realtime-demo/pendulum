e18ed83faa961a56963d852423f296f8896a524f
LanderU <lander.usategui@gmail.com>
2019-09-28 21:59:04 +0200

    Fix uncrustify issues:
    * Adapt uncrustify for pendulum_controller.hpp
    
    Signed-off-by: LanderU <lander.usategui@gmail.com>

--- pendulum_controller/pendulum_controller/include/pendulum_controller/pendulum_controller.hpp
+++ pendulum_controller/pendulum_controller/include/pendulum_controller/pendulum_controller.hpp
@@ -18,16 +18,19 @@
 #include "pendulum_msgs/msg/joint_command.hpp"
 #include "pendulum_msgs/msg/joint_state.hpp"
 
-namespace pendulum {
+namespace pendulum
+{
 
-class PendulumController {
+class PendulumController
+{
 public:
-    virtual void update_setpoint_data(const pendulum_msgs::msg::JointCommand &msg) = 0;
-    virtual void update_sensor_data(const pendulum_msgs::msg::JointState &msg) = 0;
-    virtual void update_command_data(pendulum_msgs::msg::JointCommand &msg) = 0;
-    virtual void update() = 0;
+  virtual void update_setpoint_data(const pendulum_msgs::msg::JointCommand & msg) = 0;
+  virtual void update_sensor_data(const pendulum_msgs::msg::JointState & msg) = 0;
+  virtual void update_command_data(pendulum_msgs::msg::JointCommand & msg) = 0;
+  virtual void update() = 0;
 };
 
 } // namespace pendulum
 
 #endif //PENDULUM_CONTROLLER_H
+
