ddeff68737ba2dbad9630e4d14950d6f5c087e51
LanderU <lander.usategui@gmail.com>
2019-09-29 11:08:49 +0200

    Fix pendulum_controller_node.hpp:
    * Fix include order
    * Uncrustify style
    * Do not use namespace
    
    Signed-off-by: LanderU <lander.usategui@gmail.com>

--- pendulum_controller/pendulum_controller_node/include/pendulum_controller_node/pendulum_controller_node.hpp
+++ pendulum_controller/pendulum_controller_node/include/pendulum_controller_node/pendulum_controller_node.hpp
@@ -1,4 +1,4 @@
-// Copyright 2019
+// Copyright 2019 Carlos San Vicente
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,29 +12,28 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef PENDULUM__CONTROLLER_NODE_HPP_
-#define PENDULUM__CONTROLLER_NODE_HPP_
+#ifndef PENDULUM_CONTROLLER_NODE__PENDULUM_CONTROLLER_NODE_HPP_
+#define PENDULUM_CONTROLLER_NODE__PENDULUM_CONTROLLER_NODE_HPP_
+
+#include <pendulum_msgs/msg/rttest_results.hpp>
 
 #include <memory>
 #include <string>
+
+#include "rcutils/logging_macros.h"
+
+#include "pendulum_msgs/msg/joint_command.hpp"
+#include "pendulum_msgs/msg/joint_state.hpp"
+#include "pendulum_controller_node/visibility_control.hpp"
+#include "pendulum_controller/pendulum_controller.hpp"
 
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp/publisher.hpp"
 #include "rclcpp_lifecycle/lifecycle_node.hpp"
 #include "rclcpp_lifecycle/lifecycle_publisher.hpp"
-#include "rcutils/logging_macros.h"
 
 #include "lifecycle_msgs/msg/transition_event.hpp"
 #include "lifecycle_msgs/msg/transition.hpp"
-
-#include "pendulum_msgs/msg/joint_command.hpp"
-#include "pendulum_msgs/msg/joint_state.hpp"
-#include <pendulum_msgs/msg/rttest_results.hpp>
-
-#include "pendulum_controller/pendulum_controller.hpp"
-#include "pendulum_controller_node/visibility_control.hpp"
-
-using namespace std::chrono_literals;
 
 namespace pendulum
 {
@@ -42,66 +41,65 @@ namespace pendulum
 class PendulumControllerNode : public rclcpp_lifecycle::LifecycleNode
 {
 public:
-    COMPOSITION_PUBLIC
-    explicit PendulumControllerNode(const rclcpp::NodeOptions & options)
-    : rclcpp_lifecycle::LifecycleNode("Controller", options)
-    { }
+  COMPOSITION_PUBLIC
+  explicit PendulumControllerNode(const rclcpp::NodeOptions & options)
+  : rclcpp_lifecycle::LifecycleNode("Controller", options)
+  {}
 
-    COMPOSITION_PUBLIC PendulumControllerNode(
-      const std::string & node_name,
-      std::unique_ptr<PendulumController> controller,
-      std::chrono::nanoseconds publish_period,
-      const rclcpp::QoS & qos_profile,
-      const rclcpp::QoS & setpoint_qos_profile,
-      const rclcpp::NodeOptions & options);
+  COMPOSITION_PUBLIC PendulumControllerNode(
+    const std::string & node_name,
+    std::unique_ptr<PendulumController> controller,
+    std::chrono::nanoseconds publish_period,
+    const rclcpp::QoS & qos_profile,
+    const rclcpp::QoS & setpoint_qos_profile,
+    const rclcpp::NodeOptions & options);
 
-    void on_sensor_message(const pendulum_msgs::msg::JointState::SharedPtr msg);
-    void on_pendulum_setpoint(
-      const pendulum_msgs::msg::JointCommand::SharedPtr msg);
-      /// Retrieve the command calculated from the last sensor message.
-      // \return Command message
-    const pendulum_msgs::msg::JointCommand & get_next_command_message() const;
-    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-      on_configure(const rclcpp_lifecycle::State &);
-    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-      on_activate(const rclcpp_lifecycle::State &);
-    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-      on_deactivate(const rclcpp_lifecycle::State &);
-    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-      on_cleanup(const rclcpp_lifecycle::State &);
-    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-      on_shutdown(const rclcpp_lifecycle::State & state);
+  void on_sensor_message(const pendulum_msgs::msg::JointState::SharedPtr msg);
+  void on_pendulum_setpoint(
+    const pendulum_msgs::msg::JointCommand::SharedPtr msg);
+  /// Retrieve the command calculated from the last sensor message.
+  // \return Command message
+  const pendulum_msgs::msg::JointCommand & get_next_command_message() const;
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+  on_configure(const rclcpp_lifecycle::State &);
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+  on_activate(const rclcpp_lifecycle::State &);
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+  on_deactivate(const rclcpp_lifecycle::State &);
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+  on_cleanup(const rclcpp_lifecycle::State &);
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+  on_shutdown(const rclcpp_lifecycle::State & state);
 
-    void control_timer_callback();
+  void control_timer_callback();
 
-    /// Get the subscription's settings options.
-    rclcpp::SubscriptionOptions & get_sensor_options() {return sensor_subscription_options_;}
-    rclcpp::PublisherOptions & get_command_options() {return command_publisher_options_;}
-
+  /// Get the subscription's settings options.
+  rclcpp::SubscriptionOptions & get_sensor_options() {return sensor_subscription_options_;}
+  rclcpp::PublisherOptions & get_command_options() {return command_publisher_options_;}
 
 private:
-    std::shared_ptr<rclcpp::Subscription<
+  std::shared_ptr<rclcpp::Subscription<
       pendulum_msgs::msg::JointState>> sub_sensor_;
-    std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<
+  std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<
       pendulum_msgs::msg::JointCommand>> command_pub_;
-    std::shared_ptr<rclcpp::Subscription<
+  std::shared_ptr<rclcpp::Subscription<
       pendulum_msgs::msg::JointCommand>> setpoint_sub_;
-    std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<
+  std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<
       pendulum_msgs::msg::RttestResults>> logger_pub_;
-    std::shared_ptr<rclcpp::Subscription<
+  std::shared_ptr<rclcpp::Subscription<
       lifecycle_msgs::msg::TransitionEvent>> sub_notification_;
 
-    rclcpp::PublisherOptions command_publisher_options_;
-    rclcpp::SubscriptionOptions sensor_subscription_options_;
+  rclcpp::PublisherOptions command_publisher_options_;
+  rclcpp::SubscriptionOptions sensor_subscription_options_;
 
-    rclcpp::TimerBase::SharedPtr timer_;
-    pendulum_msgs::msg::JointCommand command_message_;
-    std::chrono::nanoseconds publish_period_ = 1000000ns;
-    std::unique_ptr<PendulumController> controller_;
-    rclcpp::QoS qos_profile_ = rclcpp::QoS(1);
-    rclcpp::QoS setpoint_qos_profile_ = rclcpp::QoS(1);
+  rclcpp::TimerBase::SharedPtr timer_;
+  pendulum_msgs::msg::JointCommand command_message_;
+  std::chrono::nanoseconds publish_period_ = std::chrono::nanoseconds(1000000);
+  std::unique_ptr<PendulumController> controller_;
+  rclcpp::QoS qos_profile_ = rclcpp::QoS(1);
+  rclcpp::QoS setpoint_qos_profile_ = rclcpp::QoS(1);
 };
 
 }  // namespace pendulum
 
-#endif  // PENDULUM__CONTROLLER_NODE_HPP_
+#endif  // PENDULUM_CONTROLLER_NODE__PENDULUM_CONTROLLER_NODE_HPP_
