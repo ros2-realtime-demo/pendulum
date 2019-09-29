71e7c3f332e93d6bebc4afb1fe17fcb233df4911
LanderU <lander.usategui@gmail.com>
2019-09-29 11:51:10 +0200

    pendulum_manual_composition.cpp:
    * Uncrustify style applied
    * Fix linter errors
    * Fix include
    * Remove namespace
    
    Signed-off-by: LanderU <lander.usategui@gmail.com>

--- pendulum_demo/src/pendulum_manual_composition.cpp
+++ pendulum_demo/src/pendulum_manual_composition.cpp
@@ -1,4 +1,4 @@
-// Copyright 2019
+// Copyright 2019 Carlos San Vicente
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,6 +14,7 @@
 
 #include <iostream>
 #include <memory>
+#include <utility>
 
 #include "rclcpp/rclcpp.hpp"
 #include "rcutils/cmdline_parser.h"
@@ -24,9 +25,6 @@
 #include "pendulum_controller_node/pendulum_controller_node.hpp"
 #include "pendulum_controller/pendulum_controller.hpp"
 #include "pendulum_controller/pid_controller.hpp"
-
-using namespace std::chrono_literals;
-using namespace pendulum;
 
 static const double DEFAULT_PID_K = 1.0;
 static const double DEFAULT_PID_I = 0.0;
@@ -65,84 +63,84 @@ void print_usage()
 
 int main(int argc, char * argv[])
 {
-    // Force flush of the stdout buffer.
-    setvbuf(stdout, NULL, _IONBF, BUFSIZ);
+  // Force flush of the stdout buffer.
+  setvbuf(stdout, NULL, _IONBF, BUFSIZ);
 
-    // Argument count and usage
-    if (rcutils_cli_option_exist(argv, argv + argc, "-h")) {
-      print_usage();
-      return 0;
-    }
+  // Argument count and usage
+  if (rcutils_cli_option_exist(argv, argv + argc, "-h")) {
+    print_usage();
+    return 0;
+  }
 
-    // // Optional argument parsing
-    // if (rcutils_cli_option_exist(argv, argv + argc, OPTION_PUBLISH_FOR)) {
-    //   period_pause_talker = std::chrono::milliseconds(
-    //     std::stoul(rcutils_cli_get_option(argv, argv + argc, OPTION_PUBLISH_FOR)));
-    // }
-    // if (rcutils_cli_option_exist(argv, argv + argc, OPTION_PAUSE_FOR)) {
-    //   duration_pause_talker = std::chrono::milliseconds(
-    //     std::stoul(rcutils_cli_get_option(argv, argv + argc, OPTION_PAUSE_FOR)));
-    // }
+  // // Optional argument parsing
+  // if (rcutils_cli_option_exist(argv, argv + argc, OPTION_PUBLISH_FOR)) {
+  //   period_pause_talker = std::chrono::milliseconds(
+  //     std::stoul(rcutils_cli_get_option(argv, argv + argc, OPTION_PUBLISH_FOR)));
+  // }
+  // if (rcutils_cli_option_exist(argv, argv + argc, OPTION_PAUSE_FOR)) {
+  //   duration_pause_talker = std::chrono::milliseconds(
+  //     std::stoul(rcutils_cli_get_option(argv, argv + argc, OPTION_PAUSE_FOR)));
+  // }
 
-    // Pass the input arguments to rttest.
-    // rttest will store relevant parameters and allocate buffers for data collection
-    rttest_read_args(argc, argv);
-    
-    rclcpp::init(argc, argv);
-    rclcpp::executors::SingleThreadedExecutor exec;
+  // Pass the input arguments to rttest.
+  // rttest will store relevant parameters and allocate buffers for data collection
+  rttest_read_args(argc, argv);
 
-    std::chrono::milliseconds deadline_duration(10);
-    rclcpp::QoS qos_deadline_profile(10);
-    qos_deadline_profile.deadline(deadline_duration);
+  rclcpp::init(argc, argv);
+  rclcpp::executors::SingleThreadedExecutor exec;
 
-    PIDProperties pid;
-    pid.p = 0;
-    pid.i = 0;
-    std::chrono::nanoseconds update_period = 970000ns;
-    std::unique_ptr<PendulumController> pid_controller =
-            std::make_unique<PIDController>(update_period, pid);
-    auto controller_node =  std::make_shared<PendulumControllerNode>(
-            "pendulum_controller",
-            std::move(pid_controller),
-            update_period,
-            qos_deadline_profile,
-            rclcpp::QoS(1),
-            rclcpp::NodeOptions().use_intra_process_comms(true));
-    exec.add_node(controller_node->get_node_base_interface());
+  std::chrono::milliseconds deadline_duration(10);
+  rclcpp::QoS qos_deadline_profile(10);
+  qos_deadline_profile.deadline(deadline_duration);
 
-    std::chrono::nanoseconds sensor_publish_period =  960000ns;
-    std::chrono::nanoseconds physics_update_period = 1000000ns;
+  pendulum::PIDProperties pid;
+  pid.p = 0;
+  pid.i = 0;
+  std::chrono::nanoseconds update_period = std::chrono::nanoseconds(970000);
+  std::unique_ptr<pendulum::PendulumController> pid_controller =
+    std::make_unique<pendulum::PIDController>(update_period, pid);
+  auto controller_node = std::make_shared<pendulum::PendulumControllerNode>(
+    "pendulum_controller",
+    std::move(pid_controller),
+    update_period,
+    qos_deadline_profile,
+    rclcpp::QoS(1),
+    rclcpp::NodeOptions().use_intra_process_comms(true));
+  exec.add_node(controller_node->get_node_base_interface());
+
+  std::chrono::nanoseconds sensor_publish_period = std::chrono::nanoseconds(960000);
+  std::chrono::nanoseconds physics_update_period = std::chrono::nanoseconds(1000000);
 
 
-    std::unique_ptr<PendulumMotor> motor =
-            std::make_unique<PendulumMotorSim>(physics_update_period);
-    auto motor_node =  std::make_shared<PendulumMotorNode>(
-            "pendulum_motor_node",
-            std::move(motor),
-            sensor_publish_period,
-            qos_deadline_profile,
-            rclcpp::NodeOptions().use_intra_process_comms(true));
-    exec.add_node(motor_node->get_node_base_interface());
+  std::unique_ptr<pendulum::PendulumMotor> motor =
+    std::make_unique<pendulum::PendulumMotorSim>(physics_update_period);
+  auto motor_node = std::make_shared<pendulum::PendulumMotorNode>(
+    "pendulum_motor_node",
+    std::move(motor),
+    sensor_publish_period,
+    qos_deadline_profile,
+    rclcpp::NodeOptions().use_intra_process_comms(true));
+  exec.add_node(motor_node->get_node_base_interface());
 
-    // Set the priority of this thread to the maximum safe value, and set its scheduling policy to a
-    // deterministic (real-time safe) algorithm, round robin.
-    if (rttest_set_sched_priority(98, SCHED_RR)) {
-      perror("Couldn't set scheduling priority and policy");
-    }
+  // Set the priority of this thread to the maximum safe value, and set its scheduling policy to a
+  // deterministic (real-time safe) algorithm, round robin.
+  if (rttest_set_sched_priority(98, SCHED_RR)) {
+    perror("Couldn't set scheduling priority and policy");
+  }
 
-    // Lock the currently cached virtual memory into RAM, as well as any future memory allocations,
-    // and do our best to prefault the locked memory to prevent future pagefaults.
-    // Will return with a non-zero error code if something went wrong (insufficient resources or
-    // permissions).
-    // Always do this as the last step of the initialization phase.
-    // See README.md for instructions on setting permissions.
-    // See rttest/rttest.cpp for more details.
-    if (rttest_lock_and_prefault_dynamic() != 0) {
-      fprintf(stderr, "Couldn't lock all cached virtual memory.\n");
-      fprintf(stderr, "Pagefaults from reading pages not yet mapped into RAM will be recorded.\n");
-    }
+  // Lock the currently cached virtual memory into RAM, as well as any future memory allocations,
+  // and do our best to prefault the locked memory to prevent future pagefaults.
+  // Will return with a non-zero error code if something went wrong (insufficient resources or
+  // permissions).
+  // Always do this as the last step of the initialization phase.
+  // See README.md for instructions on setting permissions.
+  // See rttest/rttest.cpp for more details.
+  if (rttest_lock_and_prefault_dynamic() != 0) {
+    fprintf(stderr, "Couldn't lock all cached virtual memory.\n");
+    fprintf(stderr, "Pagefaults from reading pages not yet mapped into RAM will be recorded.\n");
+  }
 
-    exec.spin();
-    rclcpp::shutdown();
-    return EXIT_SUCCESS;
+  exec.spin();
+  rclcpp::shutdown();
+  return EXIT_SUCCESS;
 }
